43b101f8a73ce67b94315862966bda5fa02bcaf3
Franklin Henderson <fhenderson@cj.com>
2019-02-16 07:06:27 -0800

    fix empty icon warning

--- src/DataTable/icons.js
+++ src/DataTable/icons.js
@@ -2,6 +2,6 @@ import React from 'react';
 import Icon from '@mdi/react';
 import {mdiMenuUp, mdiMenuDown} from '@mdi/js';
 
-export const Empty = () => <Icon size={1} />;
+export const Empty = () => <Icon path="" size={1} />;
 export const Asc = () => <Icon path={mdiMenuUp} size={1} />;
 export const Desc = () => <Icon path={mdiMenuDown} size={1} />;
